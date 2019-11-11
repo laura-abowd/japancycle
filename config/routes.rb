a84280e1a9c71009691b3b6fa1143211e0aa2190
Naoto Suzuki <naoto.n.suzuki@gmail.com>
2019-11-11 15:38:07 +0900

    added routes, checked rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'pages#home'
+get '/dashboard', to: 'users#dashboard'
+resources :users, only: [:show]
+resources :cycles, only: [:index, :show, :new, :create, :destroy] do
+  resource :bookings, only: [:create]
 end
+end
+
