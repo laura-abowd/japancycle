654343ea576d15224c9e9744501d24a79d37356c
Laura Abowd <laura.abowd@gmail.com>
2019-11-14 15:48:56 +0900

    added cycles to users dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!
 
   def dashboard
-    @user = User.find(current_user.id)
-    @cycles = current_user.cycles
+    @cycles = current_user.bookings.map { |booking| booking.cycle }
     @bookings = current_user.bookings
   end
 end
