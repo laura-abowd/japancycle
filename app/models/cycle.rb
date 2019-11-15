a3dffbbeeb5bcc6888247e18725deb1e108ad454
Naoto Suzuki <naoto.n.suzuki@gmail.com>
2019-11-15 10:23:48 +0900

    removed newstatus

--- app/models/cycle.rb
+++ app/models/cycle.rb
@@ -2,7 +2,7 @@ class Cycle < ApplicationRecord
 
   include PgSearch::Model
   pg_search_scope :search_by_fields,
-    against: [ :brand, :color, :category, :location, :newstatus],
+    against: [ :brand, :color, :category, :location],
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
     }
@@ -12,7 +12,6 @@ class Cycle < ApplicationRecord
 
 
   validates :brand, presence: true
-  # validates :newstatus, presence: true
   validates :color, presence: true
   validates :category, presence: true
   validates :price, presence: true
